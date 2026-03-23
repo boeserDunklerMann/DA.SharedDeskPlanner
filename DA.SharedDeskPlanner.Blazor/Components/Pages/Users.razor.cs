2e7fe5f146ce6ca81eedd2f3da5215ac8daa54f9
André <andre.dunkel@t-online.de>
2026-03-23 09:51:59 +0100

    User model with DataAnnotation
    new Uer with validation

---
+++ DA.SharedDeskPlanner.Blazor/Components/Pages/Users.razor.cs
@@ -0,0 +1,73 @@
+﻿using DA.SharedDeskPlanner.WebAPI.Client;
+using DA.SharedDeskPlanner.WebAPI.Client.Api.User;
+using DA.SharedDeskPlanner.WebAPI.Client.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Kiota.Abstractions.Authentication;
+using Microsoft.Kiota.Http.HttpClientLibrary;
+
+namespace DA.SharedDeskPlanner.Blazor.Components.Pages
+{
+	/// <ChangeLog>
+	/// <Create Datum="04.03.2026" Entwickler="DA" />
+	/// <Change Datum="15.03.2026" Entwickler="DA">User creation</Change>
+		/// </ChangeLog>
+	public partial class Users : ComponentBase, IDisposable
+	{
+		private ApiClient? apiClient;
+		public bool Loading { get; private set; } = false;
+		/// <summary>
+		/// Usermodel for creating a new one
+		/// </summary>
+		//[SupplyParameterFromForm]
+		public Model.User NewUser { get; set; } = new();
+		//public string? NewUserLastname { get; set; }
+		//public string? NewUserFirstname { get; set; }
+		//public string? NewUserName { get; set; }
+		public List<User>? UserList { get; private set; }
+		protected override async Task OnInitializedAsync()
+		{
+			if (apiClient == null)	// TODO DA: das hier in eine Basisklasse auslagern
+			{
+				var authProvider = new AnonymousAuthenticationProvider();
+				var adapter = new HttpClientRequestAdapter(authProvider);
+				apiClient = new ApiClient(adapter);
+			}
+			if (!Loading)
+			{
+				try
+				{
+					Loading = true;
+					UserList = await apiClient.Api.User.GetAsync();
+				}
+				finally
+				{
+					Loading = false;
+				}
+			}
+		}
+		private async Task NewUserSubmittedAsync()
+		{
+			// Diese Methode wird NUR aufgerufen, wenn alle Felder valide sind.
+			if (Loading)
+				return;
+			try
+			{
+				Loading = true;
+				var postBody = new UserRequestBuilder.UserPostRequestBody() { User = new() };
+				postBody.User.LastName = NewUser.LastName;
+				postBody.User.FirstName = NewUser.FirstName;
+				postBody.User.Name = NewUser.Name;
+				await apiClient!.Api.User.PostAsync(postBody);
+				navMgr.NavigateTo(nameof(Users), true);
+			}
+			finally
+			{
+				Loading = false;
+			}
+		}
+
+		public void Dispose()
+		{
+		}
+	}
+}
--- DA.SharedDeskPlanner.Model/User.cs
+++ DA.SharedDeskPlanner.Model/User.cs
@@ -1,20 +1,26 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace DA.SharedDeskPlanner.Model
 {
 	/// <ChangeLog>
 	/// <Create Datum="18.02.2026" Entwickler="DA" />
+	/// <Change Datum="23.03.2026" Entwickler="DA">DataAnnotations added</Change>
 	/// </ChangeLog>
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable. AD: Darum kümmert sich EFCore
 	public class User : BaseModel
 	{
+		[Required(ErrorMessage = "Vorname darf nicht leer sein.")]
+		[StringLength(200, MinimumLength = 2)]
 		public string? FirstName { get; set; }
+		[Required(ErrorMessage = "Nachname darf nicht leer sein.")]
+		[StringLength(200, MinimumLength = 2)]
 		public string? LastName { get; set; }
 
 		#region Overrides
 		public override bool Equals(object? obj)
 		{
-			if (obj==null || !(obj is User)) return false;
+			if (obj == null || !(obj is User)) return false;
 			return ID == ((User)obj).ID;
 		}
 		public override int GetHashCode()
